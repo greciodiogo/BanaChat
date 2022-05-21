e3d99d5087407172e0e0e60dc5cfb6490724a980
greciodiogo <fonebahia8@gmail.com>
2022-05-21 11:54:11 +0100

    I wrapped the app.js file with the react router. Have now to Routes, the Home and Login

---
+++ reactjs/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Home';
+import Login from './Login';
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route exact path='/' element={<Home/>}/>
+    <Route exact path='/Login' element={<Login/>}/>
+  </Routes> 
+  </BrowserRouter>
+
+  );
+}
+
+export default App;
