28aafb7a8d86a73384a262dea9bd1ab734269b09
greciodiogo <fonebahia8@gmail.com>
2022-05-21 13:00:35 +0100

    this is the top sidebar and base to sidebarChat component

---
+++ reactjs/src/Sidebar.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './Sidebar.css'
+import { Avatar, IconButton } from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import SidebarChat from './SidebarChat'
+function Sidebar() {
+  const [rooms,setRooms]=useState([])
+
+  return (
+    <div className='sidebar'>
+      <div className="sidebar__header">
+          <Avatar/>
+          <div className='sidebar__headerRight'>
+            <IconButton>
+                <DonutLargeIcon/>
+            </IconButton>
+            <IconButton>
+                <ChatIcon />
+            </IconButton>
+            <IconButton>
+                <MoreVertIcon/>
+            </IconButton>
+          </div>
+      </div>
+      <div className="sidebar__search">
+            <div className='sidebar__searchContainer'>          
+                <SearchOutlined/>
+                <input placeholder='Search or start new chat' type="text"/>
+            </div>
+      </div>
+      <div className="sidebar__chats">
+          <SidebarChat addNewChat/>
+          <SidebarChat/>
+          <SidebarChat/>
+          <SidebarChat/>
+      </div>
+    </div>
+  )
+}  
+
+export default Sidebar
