574b486530529fb6050d9cc52fa8ac22dad26ca3
greciodiogo <fonebahia8@gmail.com>
2022-05-31 21:04:08 +0100

    All information came from server. I broke the Chat Component in others Components

--- reactjs/src/Chat.js
+++ reactjs/src/Chat.js
@@ -1,27 +1,62 @@
-import { Avatar, IconButton } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
 import MoreVert from '@material-ui/icons/MoreVert'
 import SearchOutlined from '@material-ui/icons/SearchOutlined'
 import AttachFile from '@material-ui/icons/AttachFile'
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import MicIcon from '@material-ui/icons/Mic'
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './Chat.css'
 
 import Picture from './assets/me.jpg'
+import Message from './Message'
+
 function Chat() {
-    const [seed, setSeed] = useState("");
-    const [input, setInput] = useState("");
+    // const [seed, setSeed] = useState("");
+    const [newMessage, setNewMessage] = useState("");
+    const [currentChat, setCurrentChat] = useState(null);
+    const [messages, setMessages] = useState([]);
+    // const [conversation, setConversation] = useState([]);
 
-    useEffect (() => {
-        setSeed(Math.floor(Math.random() * 5000));
-
-    }, []);
-
-    const sendMessage = (e) => {
+    const sendMessage = async (e) => {
         e.preventDefault();
-        console.log("You typed >>> ",input)
-        setInput("")
+        setNewMessage("")
+        const message = {
+            sender: '62927057f98a4a2bdd900fbe',
+            text: newMessage,
+            conversationId: '6294d792eafcdd08dbcd2bbd'
+        }
+        try{
+            const res = await axios.post('http://localhost:5000/api/messages',message)
+            setMessages([...messages,res.data])
+        }catch(err){
+            console.log("Erro no post message",err)
+        }
     }
+
+    // useEffect(()=>{
+    //     const getConversations = async () => {
+    //         try{
+    //             const res = await axios.get("http://localhost:5000/api/conversations/")
+    //             setConversation(res.data)
+    //             console.log(res)
+    //         }catch{
+    //             console.log("erro na requisição da api")
+    //         }
+    //     }
+    //     getConversations()
+    // },[])
+    useEffect(()=>{
+        const getMessages = async () => {
+            try{
+                const res = await axios.get("http://localhost:5000/api/messages/6294d792eafcdd08dbcd2bbd")
+                setMessages(res.data)
+            }catch(err){
+                console.log("Erro na requisiçao das mensagens "+err)
+            }
+        }
+        getMessages()
+    },[])
   return (
     <div className='chat'>
         <div className='chat__header'>
@@ -31,48 +66,23 @@ function Chat() {
                 <p>Last seen at ...</p>
             </div>
             <div className='chat__headerRight'>
-                <IconButton>
-                    <SearchOutlined />
-                </IconButton>
-                <IconButton>
-                    <AttachFile />
-                </IconButton>
-                <IconButton>
-                    <MoreVert />
-                </IconButton>
-
+                <IconButton><SearchOutlined /></IconButton>
+                <IconButton><AttachFile /></IconButton>
+                <IconButton><MoreVert /></IconButton>
             </div>
         </div>
         <div className='chat__body'>
-            <p className={`chat__message ${false && 'chat__reciever'}`}>
-            {/* <span className='chat__name'>Grécio Diogo</span> */}
-            Hey Guys
-            <span className='chat__timestamp'>3:15pm</span>
-            </p>
-            <p className={`chat__message ${false && 'chat__reciever'}`}>
-            {/* <span className='chat__name'>Grécio Diogo</span> */}
-            Hey Guys
-            <span className='chat__timestamp'>3:15pm</span>
-            </p>
-            <p className={`chat__message ${true && 'chat__reciever'}`}>
-            {/* <span className='chat__name'>Grécio Diogo</span> */}
-            Hey Guys
-            <span className='chat__timestamp'>3:15pm</span>
-            </p>
-            <p className={`chat__message ${true && 'chat__reciever'}`}>
-            {/* <span className='chat__name'>Grécio Diogo</span> */}
-            What are you doing?
-            <span className='chat__timestamp'>3:15pm</span>
-            </p>
+            {messages.map((msg,index)=>(
+                <Message message={msg} owner={ msg.sender === '62927057f98a4a2bdd900fbe' } key={index}/>
+            ))}
         </div>
         
         <div className='chat__footer'>
             <InsertEmoticonIcon/>
             <form>
-                <input value={input} onChange={e => setInput(e.target.value)} 
+                <input value={newMessage} onChange={e => setNewMessage(e.target.value)} 
                 type="text" placeholder='Type a message' />
-                <button onClick={sendMessage}
-                type='submit'>Send a message</button>
+                <button onClick={sendMessage} type='submit'>Send a message</button>
             </form>
             <MicIcon />
         </div>
