d82d40f5e2396adc6708d8383bf34d3f4a2737f4
greciodiogo <fonebahia8@gmail.com>
2022-05-31 21:09:06 +0100

    All the Information came from server

--- reactjs/src/SidebarChat.js
+++ reactjs/src/SidebarChat.js
@@ -1,34 +1,47 @@
-import { Avatar } from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
-import './SidebarChat.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./SidebarChat.css";
+import Conversation from "./Conversation";
 
-function SidebarChat({addNewChat}) {
+function SidebarChat({ addNewChat }) {
+  // const [username, setUsername] = useState('')
+  const [conversation, setConversation] = useState([]);
   //processo aleatorio de seleção dos icons avatares
-  const [seed, setSeed] = useState('') 
-  useEffect (() => {
-    setSeed(Math.floor(Math.random() * 5000));
-  }, [])
+  // const [seed, setSeed] = useState('')
+  const currentId = "62927057f98a4a2bdd900fbe"
+  
+  useEffect(() => {
+    // setSeed(Math.floor(Math.random() * 5000));
+    const getConversations = async () => {
+      
+      try {
+        const url = "http://localhost:5000/api/conversations/"+currentId;
+        const res = await axios.get(url);
+        console.log(res.data);
+        setConversation(res.data);
+      } catch (err) {
+        console.log("erro");
+      }
+    };
+    getConversations();
+  }, [currentId]);
   const createChat = () => {
     const roomName = prompt("Please enter name for chat");
     if (roomName) {
       //do some clever database stuff...
     }
-
-  }
+  };
   return !addNewChat ? (
-    <div className='sidebarChat'>
-      <Avatar/>
-      <div className='sidebarChat__info'>
-        <h2>Santos Diogo</h2>
-        <p>I got it</p>
-      </div>
+    <>
+    {conversation.map((c,index)=>(
+      <Conversation  currentId={currentId} conversation={c} key={index}/>
+      ))}
+    </>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new Chat</h2>
     </div>
-  ): (
-      <div onClick={createChat}
-      className="sidebarChat">
-        <h2>Add new Chat</h2>
-      </div>
   );
 }
 
-export default SidebarChat
+export default SidebarChat;
